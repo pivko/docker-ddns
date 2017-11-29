19d654971179906215ecb345b7e4dd9f4e2bc776
Michal Piwoni <michal.piwoni@gmail.com>
2017-11-29 15:35:34 +0100

    Update setup.sh
    ADD NS env

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 [ -z "$SHARED_SECRET" ] && echo "SHARED_SECRET not set" && exit 1;
 [ -z "$ZONE" ] && echo "ZONE not set" && exit 1;
 [ -z "$RECORD_TTL" ] && echo "RECORD_TTL not set" && exit 1;
+[ -z "$NS" ] && NS="localhost."
 
 if [ ! -f /var/cache/bind/$ZONE.zone ]
 then
@@ -21,14 +22,14 @@ EOF
 	cat > /var/cache/bind/$ZONE.zone <<EOF
 \$ORIGIN .
 \$TTL 86400	; 1 day
-$ZONE		IN SOA	localhost. root.localhost. (
+$ZONE		IN SOA	${NS} admin.${NS} (
 				74         ; serial
 				3600       ; refresh (1 hour)
 				900        ; retry (15 minutes)
 				604800     ; expire (1 week)
 				86400      ; minimum (1 day)
 				)
-			NS	localhost.
+			NS	${NS}
 \$ORIGIN ${ZONE}.
 \$TTL ${RECORD_TTL}
 EOF
